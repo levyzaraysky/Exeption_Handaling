323854bacbfb7a0bbb9315600ebd0fcc2e52dfc6
levyzaraysky <levyz@piter.org>
2025-03-07 15:42:46 -0800

    as

---
+++ Exeption_Handaling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program {
+    public static void Main (string[] args) {
+        Console.WriteLine("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Sign: ");
+        string sign = Console.ReadLine();
+        Console.WriteLine("");
+
+        switch (sign) {
+            case "+" :
+                Console.WriteLine(num1 + num2);
+                break;
+            case "-": 
+                Console.WriteLine(num1 - num2);
+                break;
+            case "*":
+                Console.WriteLine(num1 * num2);
+                break;
+            case "/":
+                Console.WriteLine(num1 / num2);
+                break;
+        }
+        
+    }
+}
